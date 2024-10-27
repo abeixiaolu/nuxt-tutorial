0b3ebf9d20e3eff1483abe33ff342b2a38d293dd
xiaoluabei <xiaoluabei@gmail.com>
2024-10-27 10:52:48 +0800

    study: assets chapter

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>This is an auto-imported component</div>
+    <img src="~/assets/xiaolu.jpg" alt="beautiful girl" width="600px" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  border-radius: 10px;
+  border: 4px solid $secondary;
+}
+</style>
