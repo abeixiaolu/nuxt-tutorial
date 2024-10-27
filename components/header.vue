0811513262b2dda553359a35876a68229a718dfc
xiaoluabei <xiaoluabei@gmail.com>
2024-10-27 11:42:20 +0800

    study: routing chapter

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,12 @@
 <template>
   <header>
-    <h1>Header</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
   </header>
 </template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  validate: (params) => {
+    if (/^\d+$/.test(params.id)) return true;
+    return {
+      statusCode: 404,
+      statusMessage: "Post not found",
+    };
+  },
+});
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Posts {{ route.params.id }}</h1>
+  </div>
+</template>
